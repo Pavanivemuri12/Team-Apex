b1bb206277d56772c14b11ebd4a4c88872356958
Pavanivemuri12 <153835795+Pavanivemuri12@users.noreply.github.com>
2024-12-10 23:09:26 +0530

    DAY-7:Not-found page

---
+++ NextJsWeek-2/practice-2/src/app/not-found.tsx
@@ -0,0 +1,9 @@
+export default function NotFound()
+{
+    return(
+        <div className=" flex flex-col  justify-center items-center h-screen">
+        <h2>Page not found</h2>
+        <p>Could not find requested Resource</p>
+  </div> 
+   )
+}
