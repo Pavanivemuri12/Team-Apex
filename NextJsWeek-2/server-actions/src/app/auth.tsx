945ff8c26abed08135fad259dfde2ee4f619bf50
Pavanivemuri12 <153835795+Pavanivemuri12@users.noreply.github.com>
2024-12-14 22:14:51 +0530

    Week-2:DAY-5 Next-auth using Google

--- NextJsWeek-2/server-actions/src/app/auth.tsx
+++ NextJsWeek-2/server-actions/src/app/auth.tsx
@@ -2,7 +2,7 @@
 
 import NextAuth from "next-auth";
 import GitHubProvider from "next-auth/providers/github";
-
+import Google from 'next-auth/providers/google'
 
 export const { auth ,handlers:{GET,POST}} = NextAuth({
     providers:[
@@ -10,6 +10,10 @@ export const { auth ,handlers:{GET,POST}} = NextAuth({
             clientId:process.env.GIT_CLIENT_ID,
             clientSecret:process.env.GIT_SECRET
 
+        }),
+        Google({
+            clientId:process.env.GOOGLE_CLIENT_ID,
+            clientSecret:process.env.GOOGLE_SECRET_KEY
         })
     ],
    secret: process.env.AUTH_SECRET
--- NextJsWeek-2/server-actions/src/app/page.tsx
+++ NextJsWeek-2/server-actions/src/app/page.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 
 import { redirect } from 'next/navigation'
 import {auth} from './auth'
+import Link from 'next/link'
 
 
 const LandingPage = async() => {
@@ -10,11 +11,21 @@ const LandingPage = async() => {
 const session = await auth()
 
 if(!session){
-  redirect('/api/auth/signin')
+  redirect('api/auth/signin')
 }
 
   return (
-    <div>Welcome to My Application</div>
+    <div className=''>
+      <div className='text-xl'>
+    Welcome to My Application
+</div>
+   <Link href="/api/auth/signout">
+    <button className='bg-red-500 hover:bg-red-700 text-white text-xl'>
+    Logout
+    </button>
+    </Link>
+    </div>
+    
   )
 }
 
