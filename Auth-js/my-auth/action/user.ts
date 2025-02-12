e2d9ae56259209b065ca03753cf57f9cce6d6eae
Pavanivemuri12 <153835795+Pavanivemuri12@users.noreply.github.com>
2025-02-12 23:50:55 +0530

    authjs5

---
+++ Auth-js/my-auth/action/user.ts
@@ -0,0 +1,36 @@
+"use server";
+
+import connectDB from "@/lib/db";
+import { User } from "@/models/User";
+import { redirect } from "next/navigation";
+
+const register = async (formData: FormData) => {
+    const firstName = formData.get('firstname') as string;
+    const lastName = formData.get('lastname') as string;
+    const email = formData.get('email') as string;
+    const password = formData.get('password') as string;
+    console.log(firstName);
+    console.log(lastName);
+    console.log(email);
+    console.log(password);
+
+    if (!firstName || !lastName || !email || !password ){
+        throw new Error("Please fill all fields");
+    }
+
+    await connectDB();
+
+
+    //existing user
+    const existingUser = await User.findOne({email})
+    if (existingUser) throw new Error("User already exists");
+
+    await User.create({firstName,lastName,email,password});
+    console.log(`User created successfully❤️🙌`)
+    redirect('/login')
+
+};
+
+
+
+export {register};
